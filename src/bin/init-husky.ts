0e7dbee1091ae99eadce497bfe67ce2e621c78c1
Gustavo Mahecha <gmahechas@outlook.com>
2023-12-25 21:37:59 -0500

    chore: files

--- src/bin/init-husky.ts
+++ src/bin/init-husky.ts
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 import fs from 'fs';
-import husky from 'husky';
+import { add, install } from 'husky';
 import path from 'path';
 
 const appPath = fs.realpathSync(process.cwd());
 const huskyPath = path.join(appPath, '.husky');
-husky.install(huskyPath);
+install(huskyPath);
 
 const preCommitPath = path.join(huskyPath, 'pre-commit');
 if (fs.existsSync(preCommitPath)) {
 	fs.unlinkSync(preCommitPath);
 }
-husky.add(preCommitPath, 'pnpm lint-staged');
+add(preCommitPath, 'pnpm lint-staged');
 
 const commitMsgPath = path.join(huskyPath, 'commit-msg');
 if (fs.existsSync(commitMsgPath)) {
 	fs.unlinkSync(commitMsgPath);
 }
-husky.add(commitMsgPath, 'pnpm commitlint');
+add(commitMsgPath, 'pnpm commitlint');
